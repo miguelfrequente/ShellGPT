dd45a28920660bef16b8ed777727fba00f5c53fd
miguelfrequente <mike@msq.vc>
2025-02-14 16:01:12 -0800

    Changed the get_response function to use structered output.

--- llm_wrapper.py
+++ llm_wrapper.py
@@ -6,17 +6,42 @@ import os
 
 import subprocess
 
+from pydantic import BaseModel
+from ollama import chat
+
 url = "http://localhost:11434/api/chat"
 
 system_prompt = """
 You are an expert using the command line interface with the bash and zsh shell. 
-The user will ask you how to do particular things with the command line interface and you will provide to things:
+The user will ask you how to do particular things with the command line interface and you will provide two things:
 First, the command that will fulfill the user's intent.
-Second, the explanation of the command and the options that you used.
+Second, an explanation of the command and the options that are intended to be used.
 """
-            
+
+
+class CommandResponse(BaseModel):         
+    command: str
+    explanation: str
 
 def get_response(prompt):
+    response = chat(
+        messages=[
+            {
+                "role": "system",
+                "content": system_prompt
+                },
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ],
+        model="llama3",
+        format=CommandResponse.model_json_schema(),
+    )
+
+    return CommandResponse.model_validate_json(response.message.content)
+
+def get_response_o(prompt):
     input = {
         "model": "llama3",
         "messages": [
@@ -38,6 +63,13 @@ def get_response(prompt):
 
     response = requests.post(url, headers=headers, json=input)
     return response.json()["message"]["content"]
+
+def check_ollama_running():
+    try:
+        response = requests.get(url)
+        return response.status_code == 200
+    except requests.exceptions.ConnectionError:
+        raise Exception("Ollama is not running. Please start the server by running `ollama` in the terminal.")
 
 
 
@@ -63,12 +95,28 @@ def validate_shell_arguments(argv):
 
 
 if __name__ == "__main__":
+    check_ollama_running()
 
     user_input = validate_shell_arguments(sys.argv)
 
-    print(get_response(user_input))
+    ollama_response = get_response(user_input)
 
+    print("> Command to be executed according to your request:")
+    print("")
+    print(ollama_response.command)
+    print("")
+    print("> Explanation of the command:")
+    print("")
+    print(ollama_response.explanation.strip())
+
+    print("")
     user_feedback = input("Should the command be executed?[yes/no]")
+
+    if user_feedback == "yes":
+        print("Executing command...")
+        result = subprocess.run(ollama_response.command, shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        print(result.stdout)
+        #os.system(ollama_response.command)
 
     
 
