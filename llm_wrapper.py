db02cb437b4a73752519d2d7f8cc13232f9189d4
miguelfrequente <mike@msq.vc>
2025-02-14 17:49:36 -0800

    introduced the SystemsPrompt class and Engine class. Added a loop for recurring prompting.

--- llm_wrapper.py
+++ llm_wrapper.py
@@ -6,41 +6,113 @@ import os
 
 import subprocess
 
+import platform 
+
 from pydantic import BaseModel
 from ollama import chat
 
 url = "http://localhost:11434/api/chat"
 
-system_prompt = """
-You are an expert using the command line interface with the bash and zsh shell. 
-The user will ask you how to do particular things with the command line interface and you will provide two things:
-First, the command that will fulfill the user's intent.
-Second, an explanation of the command and the options that are intended to be used.
-"""
+
+class SystemPrompts:
+    platform = "bac"
+
+    @classmethod
+    def decision_maker(cls):
+        return f"""
+            You are an expert using the command line interface with the bash and zsh shell. 
+            Also, you are an expert regarding the topics of IT, computer science and software engineering.
+            The user will ask you particular questions and your objective is to decide whether it is necessary to use the command line interface to fulfill the user's intent.
+            You can only provide two answers: Yes or No.
+
+            The user uses the platform: {cls.platform}
+        """
+
+    @classmethod
+    def information_provider(cls):
+        return f"""
+            You are an expert using the command line interface with the bash and zsh shell. 
+            Also, you are an expert regarding the topics of IT, computer science and software engineering.
+            The user will ask you particular questions and your objective is to provide information about the topic from your own knowledge.
+            You will respond briefly and informatively.
+
+            The user uses the platform: {cls.platform}
+        """
+
+    @classmethod
+    def command_generator(cls):
+        return f"""
+            You are an expert using the command line interface with the bash and zsh shell. 
+            The user will ask you how to do particular things with the command line interface and you will provide two things:
+            First, the command that will fulfill the user's intent.
+            Second, an explanation of the command and the options that are intended to be used.
+
+            The user uses the platform: {cls.platform}
+        """
 
 
 class CommandResponse(BaseModel):         
     command: str
     explanation: str
 
-def get_response(prompt):
-    response = chat(
-        messages=[
-            {
-                "role": "system",
-                "content": system_prompt
-                },
-            {
-                "role": "user",
-                "content": prompt
-            }
-        ],
-        model="llama3",
-        format=CommandResponse.model_json_schema(),
-    )
+class Engine:
+    model = "deepseek-r1:7b" #"llama3"
+    @staticmethod
+    def get_command(prompt):
+        response = chat(
+            messages=[
+                {
+                    "role": "system",
+                    "content": SystemPrompts.command_generator()
+                    },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            model=Engine.model,
+            format=CommandResponse.model_json_schema(),
+        )
 
-    return CommandResponse.model_validate_json(response.message.content)
+        return CommandResponse.model_validate_json(response.message.content)
 
+    @staticmethod
+    def get_decision(prompt):
+        response = chat(
+            messages=[
+                {
+                    "role": "system",
+                    "content": SystemPrompts.decision_maker()
+                    },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            model=Engine.model,
+        )
+
+        return response.message.content
+
+    @staticmethod
+    def get_information(prompt):
+        response = chat(
+            messages=[
+                {
+                    "role": "system",
+                    "content": SystemPrompts.information_provider()
+                    },
+                {
+                    "role": "user",
+                    "content": prompt
+                }
+            ],
+            model=Engine.model,
+        )
+
+        return response.message.content
+
+"""
 def get_response_o(prompt):
     input = {
         "model": "llama3",
@@ -63,6 +135,7 @@ def get_response_o(prompt):
 
     response = requests.post(url, headers=headers, json=input)
     return response.json()["message"]["content"]
+"""
 
 def check_ollama_running():
     try:
@@ -71,7 +144,17 @@ def check_ollama_running():
     except requests.exceptions.ConnectionError:
         raise Exception("Ollama is not running. Please start the server by running `ollama` in the terminal.")
 
+def check_platform():
+    system_name = platform.system()
 
+    if system_name == "Linux":
+        return "Linux"
+    elif system_name == "Darwin":
+        return "macOS"
+    elif system_name == "Windows":
+        return "Windows"
+    else:
+        return "Unknown"
 
 def validate_shell_arguments(argv):
 
@@ -97,26 +180,55 @@ def validate_shell_arguments(argv):
 if __name__ == "__main__":
     check_ollama_running()
 
+    SystemPrompts.platform = check_platform()
+
     user_input = validate_shell_arguments(sys.argv)
 
-    ollama_response = get_response(user_input)
 
-    print("> Command to be executed according to your request:")
-    print("")
-    print(ollama_response.command)
-    print("")
-    print("> Explanation of the command:")
-    print("")
-    print(ollama_response.explanation.strip())
+    while True:
+        ollama_response = Engine.get_decision(user_input)
 
-    print("")
-    user_feedback = input("Should the command be executed?[yes/no]")
+        additional_context = ""
 
-    if user_feedback == "yes":
-        print("Executing command...")
-        result = subprocess.run(ollama_response.command, shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-        print(result.stdout)
-        #os.system(ollama_response.command)
+        print(ollama_response)
+
+        if ollama_response == "Yes":
+            ollama_response = Engine.get_command(user_input)
+
+            print("> Command to be executed according to your request:")
+            print("")
+            print(ollama_response.command)
+            print("")
+            print("> Explanation of the command:")
+            print("")
+            print(ollama_response.explanation.strip())
+
+            print("")
+            user_feedback = input("Should the command be executed [yes/no]? ")
+
+            if user_feedback == "yes":
+                print("Executing command...")
+                result = subprocess.run(ollama_response.command, shell = True,stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+                print(result.stdout)
+                print(result.stderr)
+
+                additional_context = result.stdout + "\n\n" + result.stderr
+
+        elif ollama_response == "No":
+            ollama_response = Engine.get_information(user_input)
+            print(ollama_response)
+            print("")
+
+        user_feedback = input("Can I help you further? ")
+
+        if "no" in user_feedback or "No" in user_feedback or "NO" in user_feedback or "n" in user_feedback or "N" in user_feedback:
+            break
+
+        user_input = additional_context + "\n" + user_feedback
+
+        print(user_input)
+
+
 
     
 
